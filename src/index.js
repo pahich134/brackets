cad02c52943f450b3b41a0a4fa0ba11565499842
Pavel Kazliuk <pahich134@gmail.com>
2020-10-25 23:24:36 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = "[]{}()<>"
+  let stack = []
+
+  for(let bracket of str) {
+    let bracketsIndex = brackets.indexOf(bracket)
+
+    if (bracketsIndex === -1){
+      continue
+    }
+
+    if(bracketsIndex % 2 === 0) {
+      stack.push(bracketsIndex + 1)
+    } else {
+      if(stack.pop() !== bracketsIndex) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0
 }
